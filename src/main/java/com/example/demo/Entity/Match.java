38751b09c1993e438c21a12fff87f2e174e7a62c
Rohith Janwadkar <rohithjanwadkar@Rohiths-MacBook-Air.local>
2025-11-10 16:10:47 -0600

    Added Message and Match Entity Layers

---
+++ src/main/java/com/example/demo/Entity/Match.java
@@ -0,0 +1,46 @@
+package com.example.demo.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "matches")
+@Builder
+public class Match {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user1_id")
+    private User user1;
+
+    @ManyToOne
+    @JoinColumn(name = "user2_id")
+    private User user2;
+
+    @Enumerated(EnumType.STRING)
+    private MatchStatus status = MatchStatus.PENDING; // PENDING, MATCHED, REJECTED
+
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public enum MatchStatus {
+        PENDING,
+        MATCHED,
+        REJECTED
+    }
+
+
+
+}
---
+++ src/main/java/com/example/demo/Entity/Message.java
@@ -0,0 +1,36 @@
+package com.example.demo.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "matches")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "match_id", nullable = false)
+    private Match match;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private Boolean isRead = false;
+    private LocalDateTime timestamp = LocalDateTime.now();
+}
